c0aa57f33dd0a445e9eea3d51926ca6a58b0d6e3
cosquerke <kcosquer@live.com>
2024-05-16 10:37:54 +0200

    details bien avancé

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Alert, ScrollView, Image } from 'react-native';
+import { View, Text, Button, Alert, ScrollView, Image, StyleSheet } from 'react-native';
 
 const baseURL = 'https://api.spoonacular.com/recipes/';
-const apiKeyQueryString = "&apiKey=4538c45f84234eb0bb890d57be2d6398"
-//const apiKeyQueryString = "&apiKey=5538cdbd824f4e70baababf038847459"
+//const api_key = "apiKey=4538c45f84234eb0bb890d57be2d6398"
+//const api_key = "&apiKey=5538cdbd824f4e70baababf038847459"
+const api_key = "&apiKey=4bfb490dcd154b098a7177a9f8b80299"
 
 const dao = {
   getInformationAboutRecipe: async (recipe_id) => {
-    const url = `${baseURL}${recipe_id}/information?includeNutrition=false${apiKeyQueryString}`;
+    const url = `${baseURL}${recipe_id}/information?includeNutrition=false${api_key}`;
     console.log(url)
     try {
       const response = await fetch(url);
@@ -23,7 +24,7 @@ const dao = {
 
   getInstructionAboutRecipe: async (recipe_id) => {
     console.log(recipe_id);
-    const url = `${baseURL}${recipe_id}/analyzedInstructions${apiKeyQueryString}`;
+    const url = `${baseURL}${recipe_id}/analyzedInstructions?${api_key}`;
     console.log('Calling URL:', url);
 
     try {
@@ -42,60 +43,80 @@ const dao = {
 class Instruction extends Component {
   render() {
     const { instruction } = this.props;
+
+    if (!instruction.steps || instruction.steps.length === 0) {
+      return null; // Ne rien rendre si "steps" n'existe pas ou est vide
+    }
+
     return (
-      <View>
-        <Text>{instruction.number}</Text>
-        <Text>{instruction.step}</Text>
+      <View style={styles.container}>
+        <Text style={styles.instructionStepTitle}>{instruction.name}</Text>
+        {instruction.steps.map((step, index) => (
+          <View key={index} style={styles.stepContainer}>
+            <Text style={styles.instructionStep}>Étape {step.number}</Text>
+            <Text style={styles.instructionText}>{step.step}</Text>
+            {step.steps && step.steps.length > 0 && (
+              <View style={styles.subStepsContainer}>
+                {step.steps.map((subStep, subIndex) => (
+                  <View key={subIndex} style={styles.subStepContainer}>
+                    <Text style={styles.instructionSubStep}>Sous-étape {subStep.number}</Text>
+                    <Text style={styles.instructionSubText}>{subStep.step}</Text>
+                  </View>
+                ))}
+              </View>
+            )}
+          </View>
+        ))}
       </View>
     );
   }
 }
+
+
 
 class ListInstructions extends Component {
   render() {
     const { liste_instructions } = this.props;
     return (
       <View>
-          <ScrollView>
-          {liste_instructions.map((step, index) => (
-            <Instruction key={index} instruction={step} />
-          ))}
-          </ScrollView>
+        {liste_instructions.map((step, index) => (
+          <Instruction key={index} instruction={step} />
+        ))}
       </View>
     );
   }
 }
 
 class Ingredient extends Component {
-    render() {
-      const { ingredient } = this.props;
-      return (
-        <View>
-          <Text>{ingredient.name}</Text>
-          <Text>{ingredient.original}</Text>
-          <Image
-            source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}` }}
-            style={{ width: 100, height: 100 }}
-          />
+  render() {
+    const { ingredient } = this.props;
+    return (
+      <View style={styles.ingredientContainer}>
+        <Image
+          source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}` }}
+          style={styles.ingredientImage}
+        />
+        <View style={styles.ingredientTextContainer}>
+          <Text style={styles.ingredientName}>{ingredient.name}</Text>
+          <Text style={styles.ingredientOriginal}>{ingredient.original}</Text>
         </View>
-      );
-    }
+      </View>
+    );
   }
-  
-  class ListIngredients extends Component {
-    render() {
-      const { liste_ingredient } = this.props;
-      return (
-        <View>
-            <ScrollView>
-            {liste_ingredient.map((ingredient, index) => (
-            <Ingredient key={index} ingredient={ingredient} />
-          ))}
-            </ScrollView>
-        </View>
-      );
-    }
+}
+
+class ListIngredients extends Component {
+  render() {
+    const { liste_ingredient } = this.props;
+    return (
+      <View style={styles.ingredientsGrid}>
+        {liste_ingredient.map((ingredient, index) => (
+          <Ingredient key={index} ingredient={ingredient} />
+        ))}
+      </View>
+    );
   }
+}
 
 class Details extends Component {
     constructor(props) {
@@ -119,28 +140,30 @@ class Details extends Component {
     }
 
     async loadInstructions(id_recipe) {
-      console.log(id_recipe);
       let infos_api = await dao.getInstructionAboutRecipe(id_recipe);
       this.setState({ instructions: infos_api });
       if(this.state.instructions != []){
           this.setState({ instructions_loaded: true });
       }
-      console.log(this.state.instructions);
   }
 
     render(){
         if(this.state.ingredients_loaded == true && this.state.instructions_loaded == true){
             return(
-            <View>
-                <Text>{this.state.infos.title}</Text>
-                <ListIngredients liste_ingredient={this.state.infos.extendedIngredients}/>
-                <ListInstructions liste_instructions={this.state.instructions[0].steps}/>
-            </View>
+              <ScrollView style={styles.container}>
+              <Text style={styles.title}>{this.state.infos.title}</Text>
+              <ListIngredients liste_ingredient={this.state.infos.extendedIngredients} />
+              <ListInstructions liste_instructions={this.state.instructions} />
+            </ScrollView>
             )
             
         }else{
+          if(!this.state.ingredients_loaded){
             this.loadInfos(this.props.id_recipe);
+          }
+          if(!this.state.instructions_loaded){
             this.loadInstructions(this.props.id_recipe);
+          }
             return(
                 <View>
                     <Text>Api NOK</Text>
@@ -153,13 +176,97 @@ class Details extends Component {
 class DetailsScreen extends Component {
     render() {
         const { navigation, route } = this.props;
-        const userInfo = route.params?.user_info || {};
+        const id_recipe_from_props = route.params?.id_recipe || {};
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Details id_recipe={"716429"}/>
+            <View style={{ flex: 1 }}>
+                <Details id_recipe={id_recipe_from_props}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  ingredientsGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  ingredientContainer: {
+    width: '48%',
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  ingredientImage: {
+    width: 100,
+    height: 100,
+  },
+  ingredientTextContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  ingredientName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  ingredientOriginal: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  instructionBlock: {
+    marginBottom: 10,
+    padding: 15,
+    backgroundColor: '#e6e6e6',
+    borderRadius: 5,
+  },
+  stepContainer: {
+    marginBottom: 10,
+  },
+  instructionStep: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  instructionStepTitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  instructionText: {
+    fontSize: 16,
+  },
+  subStepsContainer: {
+    paddingLeft: 20,
+    borderLeftWidth: 2,
+    borderLeftColor: '#ccc',
+  },
+  subStepContainer: {
+    marginTop: 10,
+  },
+  instructionSubStep: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  instructionSubText: {
+    fontSize: 14,
+  },
+});
+
+
 
 export default DetailsScreen;
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -7,203 +7,125 @@ const baseURL = 'https://api.spoonacular.com/food/ingredients/autocomplete?query
 const apiKeyQueryString = "&apiKey=4bfb490dcd154b098a7177a9f8b80299"
 
 const dao = {
-    findAllergyByName: async (ingredient_name) => {
-      const url = `${baseURL}${ingredient_name}&number=5${apiKeyQueryString}`;
-      console.log(url)
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return await response.json();
-      } catch (error) {
-        console.error('Error fetching ingredients:', error);
-        return [];
+  findAllergyByName: async (ingredient_name) => {
+    const url = `${baseURL}${ingredient_name}&number=5${apiKeyQueryString}`;
+    console.log(url)
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-    },
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching ingredients:', error);
+      return [];
+    }
+  },
+}
+
+class AllergySearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      ingredients: [],
+    };
   }
 
-  class AllergySearch extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        query: '',
-        ingredients: [],
-      };
+  handleInputChange = async (text) => {
+    this.setState({ query: text });
+    if (text.length > 0) {
+      const results = await dao.findAllergyByName(text);
+      this.setState({ ingredients: results });
+    } else {
+      this.setState({ ingredients: [] });
     }
-  
-    handleInputChange = async (text) => {
-      this.setState({ query: text });
-      if (text.length > 0) {
-        const results = await dao.findAllergyByName(text);
-        this.setState({ ingredients: results });
-      } else {
-        this.setState({ ingredients: [] });
-      }
-    };
-    render() {
-        const { query, ingredients } = this.state;
-        console.log(this.state)
-        return (
-            <View style={styles.container}>
-            <TextInput
-              style={styles.input}
-              placeholder="Search for ingredients"
-              value={query}
-              onChangeText={this.handleInputChange}
-            />
-            <FlatList
-              data={ingredients}
-              keyExtractor={(item) => item.name}
-              renderItem={({ item }) => (
-                <View style={styles.item}>
-                  <Image
-                    source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${item.image}` }}
-                    style={styles.image}
-                  />
-                  <Text style={styles.itemText}>{item.name}</Text>
-                </View>
-              )}
-            />
-          </View>
-        );
-      }
-    }
+  };
 
-class Inputs extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstname: '',
-            lastname: '',
-            allergy: '',
-            submitted: false
-        };
-        this.handleFirstname = this.handleFirstname.bind(this);
-        this.handleLastName = this.handleLastName.bind(this);
-        this.handleAllergy = this.handleAllergy.bind(this);
-    }
-
-    handleFirstname(firstname_input) {
-        this.setState({ firstname: firstname_input });
-    }
-
-    handleLastName(lastname_input) {
-        this.setState({ lastname: lastname_input });
-    }
-
-    handleAllergy(allergy_input) {
-        this.setState({ allergy: allergy_input });
-    }
-
-    handleSubmit = () => {
-        if (this.state.firstname === '' || this.state.lastname === '') {
-            Alert.alert('Erreur', 'Veuillez remplir les champs nom et prénom');
-        } else {
-            this.setState({ submitted: true }); 
-        }
-        this.props.onSubmitted(this.state);
-    }
-
-    render() {
-        if (this.state.submitted) {
-            return (
-                <View>
-                    <Text>Prénom: {this.state.firstname}</Text>
-                    <Text>Nom: {this.state.lastname}</Text>
-                    <Text>Allergie: {this.state.allergy}</Text>
-                </View>
-            );
-        }
-
-        return (
-            <View>
-                <Text>Prénom:</Text>
-                <TextInput
-                    onChangeText={this.handleFirstname} />
-
-                <Text>Nom:</Text>
-                <TextInput
-                    onChangeText={this.handleLastName} />
-
-                <Text>Allergie:</Text>
-                <TextInput
-                    onChangeText={this.handleAllergy} />
-
-                <Button title="Submit" onPress={this.handleSubmit} />
-
+  render() {
+    const { query, ingredients } = this.state;
+    return (
+      <View >
+        <TextInput
+          
+          placeholder="Search for ingredients"
+          value={query}
+          onChangeText={this.handleInputChange}
+        />
+        <FlatList
+          data={ingredients}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <View >
+              <Image
+                source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${item.image}` }}
+                
+              />
+              <Text>{item.name}</Text>
             </View>
-        );
-    }
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+class InputFields extends React.Component {
+  handlePrenomChange = (text) => {
+    this.props.onPrenomChange(text);
+  }
+
+  render() {
+    return (
+      <View >
+        <TextInput
+          placeholder="Prenom"
+          onChangeText={this.handlePrenomChange}
+        />
+      </View>
+    );
+  }
 }
 
 class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null,
-            is_ready: false
-        };
-        this.handleInputsSubmitted = this.handleInputsSubmitted.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      prenom: ''
+    };
+  }
 
-    handleInputsSubmitted = (submitted_data) => {
-        this.setState({ data: submitted_data });
-        if (this.state.firstname != '' && this.state.lastname != '') {
-            this.setState({ is_ready: true });
-        }
-      }
+  handlePrenomChange = (prenom) => {
+    this.setState({ prenom });
+  }
 
-    render() {
-        const { navigation } = this.props;
-        if(this.state.is_ready){
-            return (
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Inputs onSubmitted={this.handleInputsSubmitted} />
-                    <Button
-                        title="Go to Search"
-                        onPress={() => navigation.navigate('Search', {user_info: this.state.data})}
-                    />
-                </View>
-            )
-        }else{
-            return (
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Inputs onSubmitted={this.handleInputsSubmitted} />
-                    <AllergySearch/>
-                </View>
-            )
-        }
-        
+  render() {
+    const { navigation } = this.props;
+    console.log(this.state.prenom)
+    if (this.state.prenom != '') {
+      return (
+        <View >   
+        <InputFields
+            onPrenomChange={this.handlePrenomChange}
+          />
+          <AllergySearch/>
+          <Button
+            title="Go to Search"
+            onPress={() => navigation.navigate('Search', { user_info: this.state })}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <InputFields
+            onPrenomChange={this.handlePrenomChange}
+          />
+          <AllergySearch />
+        </View>
+      );
     }
+  }
 }
-
-const styles = StyleSheet.create({
-    container: {
-      padding: 20,
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 20,
-      paddingHorizontal: 10,
-    },
-    item: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#ccc',
-    },
-    itemText: {
-      fontSize: 18,
-      marginLeft: 10,
-    },
-    image: {
-      width: 50,
-      height: 50,
-    },
-  });
 
 export default HomeScreen;
